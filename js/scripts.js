07af91d911e6b391caa454686ec7e4461eeb3863
Ryan Duff <ryanduff613@gmail.com>
2020-03-30 14:13:51 -0700

    sort userItems array alphabetically

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,15 @@ $(document).ready(function() {
     $('#formDiv').hide();
 
     var blanks = ['item1', 'item2', 'item3']
-
     var userItems = blanks.map(function(blank){
       return $("#"+blank).val();
     });
 
+    userItems.sort();
+   
+
     alert(userItems);
-
-
-    blanks.forEach(function(blank){
-      var userItem = $('#'+blank).val();
-    });
-  
+    alert(blanks);
   
   });
 });
