cf97c746bce3417773ada91cb79cf0a946457363
Ryan Duff <ryanduff613@gmail.com>
2020-03-30 13:51:03 -0700

    add initial html, add form, take in data from form. needs additional js to implement looping

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('#formDiv').submit(function(event){
+    event.preventDefault();
+
+    $('#formDiv').hide();
+    
+    var userItems = ['item1', 'item2', 'item3']
+
+    userItems.map(function(userItem){
+      var item = $('#'+userItem).val();
+      $
+    });
+  
+    alert(userItem1);
+  });
+});
